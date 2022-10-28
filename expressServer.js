44cedfded9ddc10e307a5e63fc4888e54b4ad56c
roymansoor <roy.mansoor@gmail.com>
2022-10-28 17:34:20 -0500

    updated

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,6 @@
-const e = require('express');
+// requiring express and save it to a variable.
 const express = require('express');
-const { addListener } = require('nodemon');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -8,32 +8,18 @@ var petsfile = require('./pets.json')
 const fs = require('fs');
 const { request } = require('http');
 
-
-
 //const userInput = express.json()
 //console.log(userInput)
 
-
-
-
-
 app.get('/pets', (req, res, next)=>{
     //res.json({data: {message: 'pest is here'}})
-   
     fs.readFile('pets.json', 'utf-8', (error, data )=>{
         const pets = JSON.parse(data)
         res.status(200).json(pets)
-    
-    })
-    
-
-
- 
+    }) 
 })
-
 app.get('/pets/:id', (req, res, next)=>{
     //res.status(200).json(petsfile[req.params.id])
-
     fs.readFile('pets.json', 'utf-8', (error, data )=>{
         const pets = JSON.parse(data)
         if(req.params.id < pets.length && req.params.id >= 0){
@@ -42,20 +28,21 @@ app.get('/pets/:id', (req, res, next)=>{
         else {
             next()
         }
-    
     })
-
 })
-
-
-
-
 
 
 app.use((req, res, next)=>{
     console.error('Not Found')
-    res.status(404).json({error:{message: 'Not Found'}})
+    res.send('Page Not Found')
+    res.status(404)//.json({error:{message: 'Not Found'}})
 })
+
+
+
+
+
+
 
 
 
