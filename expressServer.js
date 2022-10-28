96a9839c031711c01a604de24ca7de7c4ffff2d1
roymansoor <roy.mansoor@gmail.com>
2022-10-28 16:33:17 -0500

    got the reading work

---
+++ expressServer.js
@@ -0,0 +1,74 @@
+const e = require('express');
+const express = require('express');
+const { addListener } = require('nodemon');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+var petsfile = require('./pets.json')
+const fs = require('fs');
+const { request } = require('http');
+
+
+
+//const userInput = express.json()
+//console.log(userInput)
+
+
+
+
+
+app.get('/pets', (req, res, next)=>{
+    //res.json({data: {message: 'pest is here'}})
+   
+    fs.readFile('pets.json', 'utf-8', (error, data )=>{
+        const pets = JSON.parse(data)
+        res.status(200).json(pets)
+    
+    })
+    
+
+
+ 
+})
+
+app.get('/pets/:id', (req, res, next)=>{
+    //res.status(200).json(petsfile[req.params.id])
+
+    fs.readFile('pets.json', 'utf-8', (error, data )=>{
+        const pets = JSON.parse(data)
+        if(req.params.id < pets.length && req.params.id >= 0){
+        res.status(200).json(pets[req.params.id])
+        }
+        else {
+            next()
+        }
+    
+    })
+
+})
+
+
+
+
+
+
+app.use((req, res, next)=>{
+    console.error('Not Found')
+    res.status(404).json({error:{message: 'Not Found'}})
+})
+
+
+
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server runnin on ${PORT}`)
+})
+
+
+
+
+
+
