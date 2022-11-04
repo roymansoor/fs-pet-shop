05b8196530f17bbd7eb68c93b05686b3f7019348
roymansoor <roy.mansoor@gmail.com>
2022-11-04 14:58:19 -0500

    save before working on restful

--- expressServer.js
+++ expressServer.js
@@ -4,24 +4,28 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-var petsfile = require('./pets.json')
+//var petsfile = require('./pets.json')
 const fs = require('fs');
 const { request } = require('http');
 
 //const userInput = express.json()
 //console.log(userInput)
+//console.log(req.path)
 
-app.get('/pets', (req, res, next)=>{
+app.get('/pets', (req, res)=>{
+
     //res.json({data: {message: 'pest is here'}})
-    fs.readFile('pets.json', 'utf-8', (error, data )=>{
+    fs.readFile('pets.json', 'utf8', (error, data )=>{
         const pets = JSON.parse(data)
+        console.log(req.path)
         res.status(200).json(pets)
     }) 
 })
 app.get('/pets/:id', (req, res, next)=>{
     //res.status(200).json(petsfile[req.params.id])
-    fs.readFile('pets.json', 'utf-8', (error, data )=>{
+    fs.readFile('pets.json', 'utf8', (error, data )=>{
         const pets = JSON.parse(data)
+        console.log(req.path)
         if(req.params.id < pets.length && req.params.id >= 0){
         res.status(200).json(pets[req.params.id])
         }
@@ -30,7 +34,6 @@ app.get('/pets/:id', (req, res, next)=>{
         }
     })
 })
-
 
 app.use((req, res, next)=>{
     console.error('Not Found')
